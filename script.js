992dfa64badcef9ca5b88a3e11eadf3cfe9e949d
anupamtiwari010 <aptiwari010@gmail.com>
2026-01-30 14:08:05 +0530

    added images to folder

---
+++ script.js
@@ -0,0 +1,23 @@
+function scrollToMenu() {
+  document.getElementById("menu").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+
+const slider = document.querySelector(".testimonial-track");
+
+slider.addEventListener("mouseenter", () => {
+  slider.style.animationPlayState = "paused";
+});
+
+slider.addEventListener("mouseleave", () => {
+  slider.style.animationPlayState = "running";
+});
+
